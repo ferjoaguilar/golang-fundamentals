ae80389d07385e093a445fde5de027945fb16fde
Fernando Aguilar <feraguilar6985@gmail.com>
2022-02-18 23:01:38 -0600

    feat(golang):pointers

---
+++ pointers.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPc pc) ping() {
+	fmt.Println(myPc.brand, "pong")
+}
+
+func (myPc *pc) duplicateRAM() {
+	myPc.ram = myPc.ram * 2
+}
+
+func main() {
+
+	a := 50
+	b := &a
+
+	fmt.Println(b)
+	fmt.Println(*b)
+
+	*b = 100
+	fmt.Println(a)
+
+	myPc := pc{ram: 16, disk: 200, brand: "DELL"}
+	fmt.Println(myPc)
+
+	myPc.ping()
+	myPc.duplicateRAM()
+
+}
